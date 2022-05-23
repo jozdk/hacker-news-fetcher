c47a4c2dc0c2b88b4301811df9a1cebdfa04ee2e
jozdk <djannikhaezo@web.de>
2022-05-23 15:34:41 +0200

    Style app via CSS in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useReducer, useCallback } from "react";
 import axios from "axios";
+import "./App.css";
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query="
 
@@ -94,16 +95,14 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-
-      <hr />
 
       {stories.isError && <p>Something went wrong...</p>}
 
@@ -129,15 +128,19 @@ const List = ({ list, onRemoveItem }) => (
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <li>
-      <span>
+    <li className="item">
+      <span style={{ width: "40%" }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: "30%" }}>{item.author}</span>
+      <span style={{ width: "10%" }}>{item.num_comments}</span>
+      <span style={{ width: "10%" }}>{item.points}</span>
+      <span style={{ width: "10%" }}>
+        <button
+          type="button"
+          onClick={() => onRemoveItem(item)}
+          className="button button-small"
+        >
           Dismiss
         </button>
       </span>
@@ -157,7 +160,7 @@ const InputWithLabel = ({ id, value, type = "text", onInputChange, isFocused, ch
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">{children}</label>
       &nbsp;
       <input
         ref={inputRef}
@@ -166,6 +169,7 @@ const InputWithLabel = ({ id, value, type = "text", onInputChange, isFocused, ch
         value={value}
         autoFocus
         onChange={onInputChange}
+        className="input"
       />
     </>
   )
@@ -173,7 +177,7 @@ const InputWithLabel = ({ id, value, type = "text", onInputChange, isFocused, ch
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
   return (
-    <form onSubmit={onSearchSubmit}>
+    <form onSubmit={onSearchSubmit} className="search-form">
       <InputWithLabel
         id="search"
         value={searchTerm}
@@ -183,7 +187,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
         <strong>Search:</strong>
       </InputWithLabel>
 
-      <button type="submit" disabled={!searchTerm}>
+      <button
+        type="submit"
+        disabled={!searchTerm}
+        className="button button-large"
+      >
         Submit
       </button>
     </form>

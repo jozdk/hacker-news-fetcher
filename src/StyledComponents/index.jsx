6b4d2aa6bbab9a125edce3063c5c86b711f9f174
jozdk <djannikhaezo@web.de>
2022-05-23 16:53:13 +0200

    Make module out of styled components

---
+++ src/StyledComponents/index.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+  
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+  color: #171212;
+`;
+
+export const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+export const StyledItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+export const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+export const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+export const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+export const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+export const StyledSearchForm = styled.form`
+  padding: 10px;
+  display: flex;
+  align-items: baseline;
+`;
+
+export const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+export const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+  font-size: 24px;
+`;
